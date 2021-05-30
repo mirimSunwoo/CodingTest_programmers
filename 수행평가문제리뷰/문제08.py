7d7c9aef10e566ea4243b2dd2383c50e5f7097f2
Leesunwoo <73941301+mirimSunwoo@users.noreply.github.com>
2021-05-31 08:52:02 +0900

    문제 08해결

--- 수행평가문제리뷰/문제08.py
+++ 수행평가문제리뷰/문제08.py
@@ -2,7 +2,7 @@
 def solution(characters):
    result = ""
    result += characters[0]
-   for i in range(len(characters)):
+   for i in range(1,len(characters)):
        if characters[i - 1] != characters[i]:
            result += characters[i+1]
    return result
