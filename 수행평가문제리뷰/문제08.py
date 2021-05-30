b2b5cf8d6c0db879dae32456a62d9d3d21ff5f1f
Leesunwoo <73941301+mirimSunwoo@users.noreply.github.com>
2021-05-31 08:51:48 +0900

    문제 08시작

---
+++ 수행평가문제리뷰/문제08.py
@@ -0,0 +1,24 @@
+#연속으로 중복되는 문자는 제외
+def solution(characters):
+   result = ""
+   result += characters[0]
+   for i in range(len(characters)):
+       if characters[i - 1] != characters[i]:
+           result += characters[i+1]
+   return result
+
+#The following is code to output test case. The code below is correct and you shall correct solution function.
+characters = "senteeeencccccceeee"
+ret = solution(characters)
+
+#Press Run button to receive output.
+print("Solution: return value of the function is", ret, ".")
+#
+#
+# arr = "abcdefg"
+# # print(arr[len(arr)]) : 에러
+# print(arr[3])
+# print(arr[6])
+# print(arr[0])
+# print(arr[-1])
+
